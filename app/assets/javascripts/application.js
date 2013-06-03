1274fb63ed6b55b41e7b7f1cf9eea7f38a978021
Robert Wünsch <robert.wuensch@widescape.net>
2013-06-03 12:47:05 +0200

    Switched "require all of the gem files" to "require only dedicated files"

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,5 @@
 //= require jquery
-//= require any_gem
+//= require ../../../vendor/assets/javascripts/any_gem/do_not_override.js
+//= require_tree .
 
 // Note that any_gem/message.js is not required here.
---
+++ vendor/assets/javascripts/any_gem/do_not_override.js
@@ -0,0 +1,3 @@
+$(function() {
+  $('#message').before('<h3>The following line should say "Success!"</h3>');
+});
