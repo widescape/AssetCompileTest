b46f59ed95eb9bacbaacf914fbebc0f296a1581e
Robert Wünsch <robert.wuensch@widescape.net>
2013-06-03 12:51:27 +0200

    Improved documentation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,7 @@
 //= require jquery
-//= require ../../../vendor/assets/javascripts/any_gem/do_not_override.js
-//= require_tree .
 
-// Note that any_gem/message.js is not required here.
+// First require each asset file from any_gem that shall be kept
+//= require ../../../vendor/assets/javascripts/any_gem/do_not_override.js
+
+// Then require all overrides and custom files (including any_gem/message.js)
+//= require_tree .
